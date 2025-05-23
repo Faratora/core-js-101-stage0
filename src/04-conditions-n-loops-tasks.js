2ab584174c22778395e099ef04c635317a3f37e2
Svetlana <brigantina@mail.ru>
2025-05-23 21:44:07 +0300

    Revert "Update 04-conditions-n-loops-tasks.js"
    This reverts commit b38dcf199be8f33a1d6dff2e295a57b0cf8db700.

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-restricted-syntax */
 /* *************************************************************************************************
  *                                                                                                *
  * Please read the following tutorial before implementing tasks:                                   *
@@ -319,15 +318,8 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(num) {
-  let result = num;
-  while (num > 9) {
-    result = result
-      .toString()
-      .split('')
-      .reduce((sum, digit) => sum + Number(digit), 0);
-  }
-  return num;
+function getDigitalRoot(/* num */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -351,18 +343,8 @@ function getDigitalRoot(num) {
  *   '{)' = false
  *   '{[(<{[]}>)]}' = true
  */
-function isBracketsBalanced(str) {
-  const stack = [];
-  const brackets = {
-    '(': ')',
-    '{': '}',
-    '[': ']',
-    '<': '>',
-  };
-
-  
-
-  return stack.length === 0;
+function isBracketsBalanced(/* str */) {
+  throw new Error('Not implemented');
 }
 
 /**
