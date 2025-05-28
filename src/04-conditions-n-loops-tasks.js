40587b443435240f3df4e2913748173708d223b9
Svetlana <brigantina@mail.ru>
2025-05-28 18:48:49 +0300

    Update 04-conditions-n-loops-tasks.js

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -503,10 +503,38 @@ function getMatrixProduct(m1, m2) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
-}
+function evaluateTicTacToePosition(position) {
+  const lines = [
+    // Rows
+    [position[0][0], position[0][1], position[0][2]],
+    [position[1][0], position[1][1], position[1][2]],
+    [position[2][0], position[2][1], position[2][2]],
+    // Columns
+    [position[0][0], position[1][0], position[2][0]],
+    [position[0][1], position[1][1], position[2][1]],
+    [position[0][2], position[1][2], position[2][2]],
+    // Diagonals
+    [position[0][0], position[1][1], position[2][2]],
+    [position[0][2], position[1][1], position[2][0]],
+  ];
 
+  //   return lines
+  //     .map((line) =>
+  //       line[0] && line.every((cell) => cell === line[0]) ? line[0] : undefined
+  //     )
+  //     .find((result) => result !== undefined);
+  // }
+
+  let winner;
+
+  lines.forEach(([line0, line1, line2]) => {
+    if (line0 && line0 === line1 && line0 === line2) {
+      winner = line0;
+    }
+  });
+
+  return winner;
+}
 module.exports = {
   getFizzBuzz,
   getFactorial,
